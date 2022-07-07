1b50f74434e4fffb994d07b0d64e553cb5f339e5
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-07 19:11:49 +0900

    (feat) Review(상품리뷰) Entity 생성

---
+++ src/main/kotlin/com/jun/shoppingmall/review/domain/Review.kt
@@ -0,0 +1,60 @@
+package com.jun.shoppingmall.review.domain
+
+import com.jun.shoppingmall.base.BaseEntity
+import com.jun.shoppingmall.review.dto.ReviewRequest
+import org.hibernate.annotations.Comment
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Lob
+
+@Entity
+class Review : BaseEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var no: Long = 0
+
+    @Comment("상품번호")
+    @Column(nullable = false)
+    var goodsNo: Int?=0
+
+    @Lob
+    @Comment("내용")
+    @Column(nullable = true)
+    var contents: String?=null
+
+    @Comment("이미지 주소")
+    @Column(length = 100, nullable = false)
+    var image: String=""
+
+    @Comment("이미지 넓이")
+    @Column(nullable = true)
+    var width: Int?=0
+
+    @Comment("이미지 높이")
+    @Column(nullable = true)
+    var height: Int?=0
+
+    companion object {
+        @JvmStatic
+        fun fromRequest(request: ReviewRequest): Review {
+            return Review().apply {
+                goodsNo = request.goodsNo
+                contents = request.contents
+                image = request.image
+                width = request.width
+                height = request.height
+            }
+        }
+    }
+
+    fun update(updatedReview: Review) {
+        this.contents = updatedReview.contents
+        this.image = updatedReview.image
+        this.height = updatedReview.height
+        this.width = updatedReview.width
+    }
+}
